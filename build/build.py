59eef7288d819222a2801e22b4ff4bcb4efa9574
viveknathani <viveknathani2402@gmail.com>
2020-12-15 21:05:50 +0530

    Add -I flag for include/

--- build/build.py
+++ build/build.py
@@ -12,5 +12,5 @@ if os.name == "nt":
 if os.path.isdir("../bin") is False:
     subprocess.run("mkdir .." + PATH_BREAK + "bin", shell=True)
 
-compile_main = "g++ .." + PATH_BREAK + "src" + PATH_BREAK + "main.cpp -o .." + PATH_BREAK + "bin" + PATH_BREAK + "maketest"
+compile_main = "g++ .." + PATH_BREAK + "src" + PATH_BREAK + "main.cpp -o .." + PATH_BREAK + "bin" + PATH_BREAK + "maketest" + " -I .." + PATH_BREAK + "include"
 subprocess.run(compile_main, shell=True)
