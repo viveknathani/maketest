4b7360a98e342315c438630f28f365dcd46f1e32
viveknathani <viveknathani2402@gmail.com>
2020-12-16 17:36:02 +0530

    Name change

---
+++ include/Export.hpp
@@ -0,0 +1,17 @@
+#ifndef EXPORT_HPP
+#define EXPORT_HPP
+
+#include <string>
+#include "Types.hpp"
+#include <vector>
+
+class Export
+{
+    public:
+        static void json(int numberOfDataPoints, int numberOfkeys, std::string keyNames[], std::vector<TestValue*> keyValues[])
+        {
+            
+        }
+};
+
+#endif
