6b4f45cda667607cd278d661e11c3fef449d879a
viveknathani <viveknathani2402@gmail.com>
2020-12-16 17:38:06 +0530

    Reflect the changes made in include/

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include "Types.hpp"
-#include "JSONer.hpp"
+#include "Export.hpp"
+#include "Generate.hpp"
 
 int main()
 {
-    std::cout << "maketest - A tool to generate test data." << std::endl;
+    std::cout << std::endl << "maketest - A tool to generate test data." << std::endl << std::endl;
     
     std::cout << "Enter number of data points you need : ";
-    int numberOfDataPoints;
+    unsigned long long numberOfDataPoints;
     std::cin >> numberOfDataPoints;
 
     std::cout << "Enter number of keys in each data point : ";
-    int numberOfKeys;
+    unsigned long long numberOfKeys;
     std::cin >> numberOfKeys;
 
     std::string keyNames[numberOfKeys];
-    TestValue** keyValues[numberOfKeys];
+    std::vector<TestValue*> keyValues[numberOfKeys];
 
-    for(int i = 0; i < numberOfKeys; i++)
+    for(unsigned long long i = 0; i < numberOfKeys; ++i)
     {
         std::cout << "Enter name of key " << i << ": ";
         std::cin >> keyNames[i];
@@ -30,17 +31,31 @@ int main()
         switch(ttype)
         {
             case TTYPE_INT : {
+                unsigned long long low, high;
+                std::cout << "Enter lower value of your range : ";
+                std::cin >> low;
+                std::cout << "Enter upper value of your range : ";
+                std::cin >> high;
+                Generator::generateInt(numberOfDataPoints, low, high, keyValues[i]);
                 break;
             }
+
             case TTYPE_FLOAT : {
+                float low, high;
+                std::cout << "Enter lower value of your range : ";
+                std::cin >> low;
+                std::cout << "Enter upper value of your range : ";
+                std::cin >> high;
+                Generator::generateFloat(numberOfDataPoints, low, high, keyValues[i]);
                 break;
             }
+
             case TTYPE_STRING : {
                 break;
             }
         }
     }
     
-    JSON::exportData(numberOfDataPoints, numberOfKeys, keyNames, keyValues);
+    Export::json(numberOfDataPoints, numberOfKeys, keyNames, keyValues);
     return 0;
 }
