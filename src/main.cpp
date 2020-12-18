d0765f9bfc73831735cfd61a4fc63a9919556892
viveknathani <viveknathani2402@gmail.com>
2020-12-18 20:19:59 +0530

    Calculate compute time

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include "Types.hpp"
+// by : Vivek Nathani
+
 #include "Export.hpp"
 #include "Generate.hpp"
+#include "Types.hpp"
+#include <chrono>
+#include <iostream>
 
 int main()
 {
     std::cout << std::endl << "maketest - A tool to generate test data." << std::endl << std::endl;
     
-    std::cout << "Enter number of data points you need : ";
+    std::cout << "How many data points you need : ";
     unsigned long long numberOfDataPoints;
     std::cin >> numberOfDataPoints;
 
@@ -18,6 +21,7 @@ int main()
     std::string keyNames[numberOfKeys];
     std::vector<TestValue*> keyValues[numberOfKeys];
 
+    unsigned int totalComputeTime = 0;
     for(unsigned long long i = 0; i < numberOfKeys; ++i)
     {
         std::cout << "Enter name of key " << i << ": ";
@@ -36,7 +40,12 @@ int main()
                 std::cin >> low;
                 std::cout << "Enter upper value of your range : ";
                 std::cin >> high;
+                auto start = std::chrono::high_resolution_clock::now();
                 Generator::generateInt(numberOfDataPoints, low, high, keyValues[i]);
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+                totalComputeTime += duration.count();
+                std::cout << "Generated " << numberOfDataPoints << " integers in " << duration.count() << "ms." << std::endl;
                 break;
             }
 
@@ -46,7 +55,12 @@ int main()
                 std::cin >> low;
                 std::cout << "Enter upper value of your range : ";
                 std::cin >> high;
+                auto start = std::chrono::high_resolution_clock::now();
                 Generator::generateFloat(numberOfDataPoints, low, high, keyValues[i]);
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+                totalComputeTime += duration.count();
+                std::cout << "Generated " << numberOfDataPoints << " real values in " << duration.count() << "ms." << std::endl;
                 break;
             }
 
@@ -55,12 +69,25 @@ int main()
                 std::string requirement;
                 fflush(stdin);
                 std::getline(std::cin, requirement);
+                auto start = std::chrono::high_resolution_clock::now();
                 Generator::generateStrings(numberOfDataPoints, requirement, keyValues[i]);
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+                totalComputeTime += duration.count();
+                std::cout << "Generated " << numberOfDataPoints << " strings in " << duration.count() << "ms.." << std::endl;
                 break;
             }
         }
+        std::cout << std::endl;
     }
     
+    auto start = std::chrono::high_resolution_clock::now();
     Export::json(numberOfDataPoints, numberOfKeys, keyNames, keyValues);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    totalComputeTime += duration.count();
+    std::cout << "Exported to JSON in "  << duration.count() << "ms." << std::endl;
+    std::cout << "Total compute time : " << totalComputeTime << "ms." << std::endl;
+    std::cout << "Checkout the release.json file in root of the current directory." << std::endl;
     return 0;
 }
