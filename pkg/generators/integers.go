581462cd3b5c00822c5064f289f688f50cd3bfe0
viveknathani <viveknathani2402@gmail.com>
2021-07-02 17:35:15 +0530

    refactor: make number generation reusable
    This is made reusable to facilitate date.go in future.

--- pkg/generators/integers.go
+++ pkg/generators/integers.go
@@ -1,11 +1,20 @@
 package generators
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
+
+// Utility function to generate a random number within a range
+func int64RandomRange(low int64, high int64) int64 {
+	return rand.Int63n(high-low) + low
+}
 
 // GenerateInts will fill up the provided array with integers in the given range.
 func GenerateInts(numberOfDataPoints uint64, low int64, high int64, arr *[]Any) {
 
+	rand.Seed(time.Now().Unix())
 	for i := uint64(0); i < numberOfDataPoints; i++ {
-		(*arr)[i] = rand.Int63n(high-low) + low
+		(*arr)[i] = int64RandomRange(low, high)
 	}
 }
