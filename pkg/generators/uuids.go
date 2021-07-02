955ac0ad92b8e75af1814d9b482554592fda2049
viveknathani <viveknathani2402@gmail.com>
2021-07-02 17:49:22 +0530

    refactor: describe the function

--- pkg/generators/uuids.go
+++ pkg/generators/uuids.go
@@ -2,6 +2,7 @@ package generators
 
 import "github.com/google/uuid"
 
+// GenerateUUIDs will fill the list with random UUIDs
 func GenerateUUIDs(numberOfDataPoints uint64, arr *[]Any) {
 
 	for i := uint64(0); i < numberOfDataPoints; i++ {
